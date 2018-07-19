d2c2af5060e5f7cedd76a78249eee256a62be01b
TTT <t597932331@163.com>
2018-07-19 18:56:11 +0800

    完善exceptionutils的测试

--- src/test/java/com/chashaotao/exceptionutils/ExceptionInfoBaseTest.java
+++ src/test/java/com/chashaotao/exceptionutils/ExceptionInfoBaseTest.java
@@ -11,6 +11,11 @@ import com.chashaotao.exceptionutils.error.ParameterException;
 
 public class ExceptionInfoBaseTest {
 
+	/**
+	 * 测试ExceptionInfoBase类是否可以将Exception错误中的信息
+	 * 提取出来，并且显示在控制台
+	 * @throws ParameterException
+	 */
 	@Test
 	public void testGetStackTrace() throws ParameterException 
 	{
@@ -25,9 +30,15 @@ public class ExceptionInfoBaseTest {
 			int index = stackTraceInfo.indexOf("by zero");
 			assertTrue("is string", index > 0);
 			System.out.println(stackTraceInfo);
+			System.out.println("==================1");
 		}
 	}
 
+	/**
+	 * 测试ExceptionInfoBase类是否可以将Exception错误中的信息
+	 * 提取出来，并且显示在控制台，并且用log4j输出到文件
+	 * @throws ParameterException
+	 */
 	@Test
 	public void testGetStackTrace2() throws ParameterException 
 	{
@@ -43,10 +54,15 @@ public class ExceptionInfoBaseTest {
 			int index = stackTraceInfo.indexOf("by zero");
 			assertTrue("is string", index > 0);
 			System.out.println(stackTraceInfo);
+			System.out.println("==================2");
 			logger.info(stackTraceInfo);
 		}
 	}
 
+	/**
+	 * 测试ExceptionInfoBase的getStackTrace方法，传入非法参数是否报错
+	 * @throws ParameterException
+	 */
 	@Test(expected = ParameterException.class)
 	public void testGetStackTraceErrorParam() throws ParameterException 
 	{
@@ -62,6 +78,7 @@ public class ExceptionInfoBaseTest {
 			int index = stackTraceInfo.indexOf("by zero");
 			assertTrue("is string", index > 0);
 			System.out.println(stackTraceInfo);
+			System.out.println("==================3");
 			logger.info(stackTraceInfo);
 		}
 	}
