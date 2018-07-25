27e18bbb16c83b286be7526b3a19df1283a9b965
TTT <t597932331@163.com>
2018-07-25 20:15:47 +0800

    add annotation

--- src/main/java/com/chashaotao/exceptionutils/ExceptionInfoBase.java
+++ src/main/java/com/chashaotao/exceptionutils/ExceptionInfoBase.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package com.chashaotao.exceptionutils;
 
 import java.io.ByteArrayOutputStream;
@@ -8,6 +11,13 @@ import com.chashaotao.exceptionutils.exceptioninfo.inter.IExceptionInfoBase;
 
 public class ExceptionInfoBase implements IExceptionInfoBase{
 
+	/**
+	 * 用来提取异常对象中的异常信息，并且返回
+	 * 
+	 * @param e 需要提取的异常对象
+	 * @return 返回异常信息
+	 * @exception ParameterException 如果传入的参数不是Exception类型将抛出
+	 */
 	public String getStackTrace(Exception e) throws ParameterException
 	{
 		if (e instanceof Exception)
